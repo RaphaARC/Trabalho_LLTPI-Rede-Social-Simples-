da5201940da438879f4655027c56670a810a980b
RaphaARC <rapha.arc12@gmail.com>
2026-04-30 19:36:45 -0300

    Simplificando ExibirUsuarios.cpp

--- core/ExibirUsuários.cpp
+++ core/ExibirUsuários.cpp
@@ -1,23 +1,8 @@
 #include<iostream>
 #include <string>
+#include <header.hpp>
 
 using namespace std;
-
-struct Usuario {
-string username;
-string email;
-int id;
-int seguidores;
-bool ativo; // true = conta ativa, false = conta suspensa
-};
-
-struct Post {
-string conteudo; // texto do post
-int id;
-int idAutor; // id do Usuario que criou o post
-int curtidas;
-bool publico; // true = vis´ıvel para todos, false = apenas seguidores
-};
 
 void exibirUsuarios(const Usuario* v, int n){
 	for(int i=0; i<n; i++) {
