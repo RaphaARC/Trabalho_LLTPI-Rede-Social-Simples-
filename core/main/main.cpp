60e4895bd7c8fc3e8517f0d316c928aa8a1ef45e
GabrielLS0135 <gabriellopes.cefetmg@gmail.com>
2026-05-01 20:20:01 -0300

    Update main.cpp
    Nova formatação, Implementação da nova função.

--- core/main/main.cpp
+++ core/main/main.cpp
@@ -7,44 +7,72 @@ using namespace std;
 int main(){
     SetConsoleTitle("EchoFeed");
 
-//Inicialização do Sistema
+	//Inicialização do Sistema
     int nUsers;
     int nPosts;
 
+	cout << endl << endl;
+
 	cout << "NUMERO DE USUARIOS: ";
     cin >> nUsers;
-    cout << endl << endl;
+    cout << endl;
 	Usuario *users = new Usuario[nUsers];
 
     cout << "NUMERO DE POSTS: ";
     cin >> nPosts;
     Post *posts = new Post[nPosts];
+    
+    cout << endl << endl;
 
-//Leitura de Usuários e Posts
+	//Leitura de Usuários e Posts
     lerUsuarios(users, nUsers);
     lerPosts(posts, nPosts, users, nUsers);
 
-//Exibição de Usuários e Posts
+	//Exibição de Usuários e Posts
+	cout << "=== EXIBINDO USUARIOS ===\n\n";
     exibirUsuarios(users, nUsers);
+    cout << "\n=== EXIBINDO POSTS ===\n\n";
     exibirPosts(posts, nPosts, users, nUsers);    
 
+	// Busca de Usuário
+	cout << "\n=== BUSCA DE USUARIO ===\n\n";
     cout << "Digite um id para buscar: ";
-    int x;
-    cin >> x;
+    int idUser;
+    cin >> idUser;
 
-    const Usuario* usuarioEncontrado = Search(users, nUsers, x);
+    Usuario* userEncontrado = buscarUsuarioporId(users, nUsers, idUser);
+    
+    if(userEncontrado != nullptr){
+    	cout << "-> Usuario encontrado!\n-> ";
+		exibirUsuarios(userEncontrado, 1);
+	} else {
+		cout << "-> O usuario de id [" << idUser << "] e inexistente.";
+	}
+	
+	cout << endl;
+	
+	// Busca de Post
+	cout << "=== BUSCA DE POST ===\n\n";
+    cout << "Digite um id de post para buscar: ";
+    int idPost;
+    cin >> idPost;
 
-    if(usuarioEncontrado != nullptr) {
-        cout << "Usuario encontrado: " << endl;
-        exibirUsuarios(usuarioEncontrado, 1);
-    } else {
-        cout << "Usuario nao encontrado." << endl;
-    }
+    Post* postEncontrado = buscarPostPorId(posts, nPosts, idPost);
+    
+    if(postEncontrado != nullptr){
+    	cout << "-> Post encontrado!\n-> ";
+		exibirPosts(postEncontrado, 1, users, nUsers);
+	} else {
+		cout << "-> O post de id [" << idPost << "] e inexistente.";
+	}
 
+	cout << endl << endl;
+	
 	delete[] users;
 	users = nullptr;
     delete[] posts;
     posts = nullptr;
 	
+	system("pause");
 	return 0;
 }
