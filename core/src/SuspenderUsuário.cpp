8218d5bd97f264bb44dc1f8ff6a2eb35cbfad89c
Gabriel Lopes <gabriellopes.cefetmg@gmail.com>
2026-05-02 13:59:26 -0300

    Update SuspenderUsuário.cpp

--- core/src/SuspenderUsuário.cpp
+++ core/src/SuspenderUsuário.cpp
@@ -4,23 +4,16 @@
 
 using namespace std;
 
-void suspenderUsuario(Usuario* u, int nUsers){
-int x; 
-
-	cout << "Digite o id do ususario que gostaria de suspender abaixo: " << endl; 
-	cin >> x; 
+void suspenderUsuario(Usuario* u){
 	
-		Usuario* usuarioEncontrado = buscarUsuarioporId(u, nUsers, x); //nUsers precisa se tornar uma variável global para ser usada nesse parâmetro
+	if(u != nullptr){
+		cout << "\n\n-> Usuario encontrado.\n" << endl; 
 	
-	if(usuarioEncontrado != nullptr){
-		cout << "\n\n==Usuario encontrado== \n" << endl; 
-	
-	if(usuarioEncontrado->ativo == false){
-		cout << "Esse usuario ja esta suspenso\n\n" << endl; 
-}
-
-	else usuarioEncontrado->ativo=false;
-}
-	else cout << "\n\n==Usuario nao encontrado==\n\n" << endl; }
-	
-	
+		if(u->ativo == false){
+			cout << "-> Esse usuario ja esta suspenso.\n\n" << endl; 
+		}
+		else u->ativo=false;
+		
+	}
+	else cout << "\n\n -> Usuario nao encontrado.\n\n" << endl; 
+}	
